89efcf3a7f2251ce8be30b32de0ed32ce247daf5
vichu-mac <victor.y.hu@gmail.com>
2016-03-21 15:16:20 -0400

    basic main menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+	void OnGUI()
+	{
+		const int buttonWidth = 84;
+		const int buttonHeight = 60;
+
+		Rect buttonRect1 = new Rect(
+			Screen.width / 4 - (buttonWidth / 2),
+			(Screen.height / 4) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+		);
+			
+		if(GUI.Button(buttonRect1,"Start"))
+		{
+			Application.LoadLevel("Song1");
+		}
+			
+		Rect buttonRect2 = new Rect(
+			(3 * Screen.width / 4) - (buttonWidth / 2),
+			(Screen.height / 4) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+		);
+
+		// Draw a button to start the game
+		if(GUI.Button(buttonRect2,"Quit"))
+		{
+			Application.Quit();
+		}
+
+
+	}
+}
