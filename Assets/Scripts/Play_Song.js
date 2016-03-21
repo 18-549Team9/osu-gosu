2c6242dc440d34f0d824413dcbc87e61087798ce
kkuan <kevinlkuan@gmail.com>
2016-03-21 16:33:43 -0400

    buttons are removed when clicked

--- Assets/Scripts/Play_Song.js
+++ Assets/Scripts/Play_Song.js
@@ -4,13 +4,22 @@ var Blueobject : GameObject;
 var Greenobject : GameObject;
 var Yellowobject : GameObject;
 
-function Start () {
 
+private var leftClickedObject : GameObject;
+private var rightClickedObject : GameObject;
+private var frontmostRaycastHit : RaycastHit2D;
+
+// It's necessary to access the SpriteRenderer of a game object to be able to access its sorting layer ID 
+// and sorting order ("Order in Layer" in the inspector)
+private var spriteRenderer : SpriteRenderer;
+
+
+function Start () {   
 }
 
 function Update () {
-	Instantiate(Redobject, transform.position + Vector3(-1,4,0), Quaternion.Euler(0,0,0));
-	Instantiate(Blueobject, transform.position + Vector3(0,4,0), Quaternion.Euler(0,0,0));
-	Instantiate(Greenobject, transform.position + Vector3(1,4,0), Quaternion.Euler(0,0,0));
-	Instantiate(Yellowobject, transform.position + Vector3(2,4,0), Quaternion.Euler(0,0,0));
+// 	Instantiate(Redobject, transform.position + Vector3(-1,4,0), Quaternion.Euler(0,0,0));
+//	Instantiate(Blueobject, transform.position + Vector3(0,4,0), Quaternion.Euler(0,0,0));
+//	Instantiate(Greenobject, transform.position + Vector3(1,4,0), Quaternion.Euler(0,0,0));
+//	Instantiate(Yellowobject, transform.position + Vector3(2,4,0), Quaternion.Euler(0,0,0));  
 }
